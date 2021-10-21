8105bc95dfcc94df63e34a547d6db68d656e6971
Altidor <altidorjb@gmail.com>
2021-10-21 05:01:44 -0400

    Added base page

--- ideal_villa/urls.py
+++ ideal_villa/urls.py
@@ -14,17 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.views.static import serve
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('',include('stock.urls')),
     # path('admin/', admin.site.urls),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
---
+++ stock/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from .views import *
+
+urlpatterns = [
+path('',index,name='index'),
+
+
+]
